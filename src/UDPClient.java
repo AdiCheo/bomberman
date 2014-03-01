2ab4be670e5c493f30661bb1ba9572906dee6f94
Adi Cheo <adi.cheo@gmail.com>
2014-03-01 18:22:29 -0500

    changes to UDPclient

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -11,15 +11,23 @@ class UDPClient {
       
       byte[] sendData = new byte[1024];
       byte[] receiveData = new byte[1024];
-      
-      String sentence = inFromUser.readLine();
-      sendData = sentence.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-      clientSocket.send(sendPacket);
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      clientSocket.receive(receivePacket);
-      String modifiedSentence = new String(receivePacket.getData());
-      System.out.println("FROM SERVER:" + modifiedSentence);
-      clientSocket.close();
+      while (true){
+	      String sentence = inFromUser.readLine();
+	      
+	      if (sentence == "q\n"){
+	    	  clientSocket.close();
+	    	  break;
+	      }
+	      
+	      sendData = sentence.getBytes();
+	      sentence = ""; // reset sentence variable
+	      
+	      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9875);
+	      clientSocket.send(sendPacket);
+	      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	      clientSocket.receive(receivePacket);
+	      String modifiedSentence = new String(receivePacket.getData());
+	      System.out.println("FROM SERVER:" + modifiedSentence);
+      }
    }
 }
