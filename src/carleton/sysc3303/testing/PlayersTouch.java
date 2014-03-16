2db6f3ef8a4a4dfa5e91de11ecd8c8cbf39795e6
ncmancuso <ncmancuso4@gmail.com>
2014-03-16 13:07:40 -0400

    setup()
    PlayersTouch test case setup() ideas?

--- src/carleton/sysc3303/testing/PlayersTouch.java
+++ src/carleton/sysc3303/testing/PlayersTouch.java
@@ -2,26 +2,48 @@ package carleton.sysc3303.testing;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.net.InetAddress;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import carleton.sysc3303.client.RunBot;
-import carleton.sysc3303.server.Run;
+import carleton.sysc3303.client.BotClient;
+import carleton.sysc3303.client.connection.IConnection;
+import carleton.sysc3303.client.connection.UDPConnection;
+import carleton.sysc3303.common.PlayerTypes;
+import carleton.sysc3303.server.GameBoard;
+import carleton.sysc3303.server.ServerBoard;
+import carleton.sysc3303.server.connection.IServer;
+import carleton.sysc3303.server.connection.UDPServer;
 
 public class PlayersTouch {
 
 	@Before
 	public void setUp() throws Exception {
-		Run server = new Run();
-		RunBot bot = new RunBot();
-		
-		//somehow run server and bots here
+		//Set up server		
+		IServer s = new UDPServer(9999, 50);
+        ServerBoard b;
+
+        b = new ServerBoard(20);
+
+        new GameBoard(s, b);
+        new Thread(s).start(); // background the server
+
+        System.out.println("Started");
+        
+        //Place bot
+        IConnection c = new UDPConnection(InetAddress.getByName("localhost"), 9999);
+
+        new BotClient(c, new File("bot.txt"), 300, PlayerTypes.PLAYER);
+
+        new Thread(c).start();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//terminate server
+		
 	}
 
 	@Test
