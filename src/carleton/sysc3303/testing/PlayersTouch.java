38cf17ebd667fa5d848dfd824bb390b06dcfc8e7
ncmancuso <ncmancuso4@gmail.com>
2014-03-16 12:35:24 -0400

    PlayersTouch

--- src/carleton/sysc3303/testing/PlayersTouch.java
+++ src/carleton/sysc3303/testing/PlayersTouch.java
@@ -6,22 +6,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import carleton.sysc3303.client.RunBot;
 import carleton.sysc3303.server.Run;
 
 public class PlayersTouch {
 
 	@Before
 	public void setUp() throws Exception {
+		Run server = new Run();
+		RunBot bot = new RunBot();
 		
+		//somehow run server and bots here
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		//terminate server
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//wait for collision...
+		
+		//when collision happens
+			//check if collision happened as expected
 	}
 
 }
