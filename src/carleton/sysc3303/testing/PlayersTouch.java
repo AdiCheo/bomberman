bf3ba662fb9202bd099b6c7e0bd10c6f0fc54144
ncmancuso <ncmancuso4@gmail.com>
2014-03-16 12:14:45 -0400

    fixed ignore

--- src/carleton/sysc3303/testing/PlayersTouch.java
+++ src/carleton/sysc3303/testing/PlayersTouch.java
@@ -2,9 +2,22 @@ package carleton.sysc3303.testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import carleton.sysc3303.server.Run;
+
 public class PlayersTouch {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
 
 	@Test
 	public void test() {
